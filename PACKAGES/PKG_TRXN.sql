4c6b934eead96e62ccd45995fd67c73e51a308c1
Ravi Sukanya <uare@protonmail.com>
2024-05-02 10:48:04 +0530

    Relase

--- PACKAGES/PKG_TRXN.sql
+++ PACKAGES/PKG_TRXN.sql
@@ -3,6 +3,5 @@ as
 g_txn_id number;
 procedure prc_do_trxn(ps_upi_id varchar2, pr_upi_id varchar2, pamount number);
 procedure prc_do_trxn(ps_mobil_no number, pr_mobil_no number, pamount number);
- --- test adding
 end;
 /
